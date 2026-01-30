9f89e72fa5d5ab6bbb8ab500f2d72f5767622db6
cds-amal <cds.sudama@gmail.com>
2026-01-30 17:05:09 -0500

    Add IMPERATIVE label to Storybook sidebar

---
+++ .storybook/manager.ts
@@ -0,0 +1,13 @@
+import { addons } from 'storybook/manager-api'
+import { create } from 'storybook/theming'
+
+const theme = create({
+  base: 'light',
+  brandTitle: '📦 IMPERATIVE',
+  brandUrl: '#',
+  brandTarget: '_self',
+})
+
+addons.setConfig({
+  theme,
+})
